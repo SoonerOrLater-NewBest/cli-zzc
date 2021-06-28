19c7c2bc2920c304ed491db8e4168705d7c81aee
SoonerOrLater-NewBest <1245515178@qq.com>
2021-06-28 16:08:17 +0800

    add base64

---
+++ src/commands/transform/base64.ts
@@ -0,0 +1,31 @@
+import { Command, flags } from '@oclif/command';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as mineType from 'mime-types';
+
+export default class Base64 extends Command {
+    static description = '转换base64';
+
+    static flags = {
+        help: flags.help({ char: 'h' }),
+        write: flags.boolean({
+            char: 'w',
+            default: false,
+            description: '将base64数据写入文件（同输入文件路径）',
+        }),
+    };
+
+    static args = [{ name: 'filePath', required: true }];
+
+    async run() {
+        const { args, flags } = this.parse(Base64);
+        const { filePath } = args;
+
+        const file = path.resolve(filePath);
+        const data = fs.readFileSync(file);
+        const base = Buffer.from(data).toString('base64');
+        const base64 = 'data:' + mineType.lookup(file) + ';base64,' + base;
+        flags.write && fs.writeFileSync(path.resolve(filePath.replace('.', '-base64.')), base64);
+        this.log(`base64: ${base64}`);
+    }
+}
