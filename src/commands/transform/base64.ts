e256c73aceba9f9d958f3ec13fdafaafdfe92284
SoonerOrLater-NewBest <1245515178@qq.com>
2021-07-01 15:06:56 +0800

    add @type

--- src/commands/transform/base64.ts
+++ src/commands/transform/base64.ts
@@ -4,7 +4,7 @@ import * as path from 'path';
 import * as mineType from 'mime-types';
 
 export default class Base64 extends Command {
-    static description = '转换base64';
+    static description = '任意文件转换base64编码';
 
     static flags = {
         help: flags.help({ char: 'h' }),
@@ -26,6 +26,6 @@ export default class Base64 extends Command {
         const base = Buffer.from(data).toString('base64');
         const base64 = 'data:' + mineType.lookup(file) + ';base64,' + base;
         flags.write && fs.writeFileSync(path.resolve(filePath.replace('.', '-base64.')), base64);
-        this.log(`base64: ${base64}`);
+        this.log(`复制后面的base64编码： ${base64}`);
     }
 }
